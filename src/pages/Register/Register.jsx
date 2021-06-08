58ed12148537d28c80f5617f8f30b5b04b065e57
Thyago Fontenele <thyago_fonte@outlook.com>
2021-06-07 23:38:45 -0300

    problema de bg e faltando logo em login e cadastro

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -9,41 +9,40 @@ function Register() {
                 <Link to="/"><img src={BackPoint} alt="seta-voltar"  className="back-point-register"/></Link>
                 <h1 id="cadastre-se">Cadastre-se</h1>
                 <br />
-                <label id="labels">Nome</label>
+                <label className="labels">Nome</label>
                 <br />
                 <input type="text"  id="input"/>
                 <br />
-                <label id="labels">E-mail</label>
+                <label className="labels">E-mail</label>
                 <br />
                 <input type="email" className="input-mail" id="input"/>
                 <br />
-                <label id="labels">CPF</label>
+                <label className="labels">CPF</label>
                 <br />
                 <input type="text" className="input-cpf" id="input"/>
                 <br />
-                <label id="labels">Data de nascimento</label><label id="telefone">Telefone</label>
+                <label className="labels-2">Data de nascimento</label><label id="telefone">Telefone</label>
                 <br />
                 <input type="date" className="date" />
                 <input type="text" className="phone-number" />
                 <br />
-                <label id="labels">Tipo de Usuário</label>
+                <label className="labels-2">Tipo de Usuário</label>
                 <br />
                 <select id="user-type">
                     <option></option>
-                    <option>Usuário</option>
+                    <option>Cliente</option>
                     <option>Entregador</option>
                 </select>
+                <label  className="labels-2">Endereço</label>
                 <br />
-                <label  id="labels">Endereço</label>
+                <input type="text"  className="input-endereco"/>
                 <br />
-                <input type="text" id="input"/>
+                <label className="labels-2">Senha</label><label className="label-confirmar-senha">Confirmação Senha</label>
                 <br />
-                <label id="labels">Senha</label><label>Confirmação Senha</label>
+                <input type="password" className="input-cadastro-senha" />
+                <input type="password" className="input-confirmar-senha"/>
                 <br />
-                <input type="password" />
-                <input type="password" />
-                <br />
-                <button>Cadastrar</button>
+                <button id="botao-cadastrar">Cadastrar</button>
             </div>
         </>
     )
