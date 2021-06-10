86b76318ffeecf97579e860e1a13a38d7e89add1
Thyago Fontenele <thyago_fonte@outlook.com>
2021-06-10 20:22:02 -0300

    css menuUser e Carrinho ok

--- src/components/Header/MenuBag/MenuBag.jsx
+++ src/components/Header/MenuBag/MenuBag.jsx
@@ -3,13 +3,31 @@ import './MenuBag.css'
 
 function MenuBag(props) {
     return (props.trigger) ? (
-        <div>
-          <nav id="bag-container">
+        <>
+          
+          <div id="bag-container">
+          
                 <div>
-                  <h1>dadsdad</h1>
+                  <h1 className="carrinho-text">Carrinho</h1>
+                  <div>
+                      <td>
+                          <tr>
+                            <p id="empty">Não há nada no seu carrinho :/</p>
+                          </tr>
+                      </td>
+                  </div>
+                 
                 </div>
-            </nav>   
-        </div>
+                <p className="limpar-text">Limpar carrinho</p>
+                
+                <div>
+                  <div className="barra-carrinho"></div>
+                  <h2 className="total-carrinho-text">Total</h2>
+                  <h2 className="total-carrinho">R$ 0,00</h2>
+                  <button className="carrinho-button">Pagar</button>
+                </div>
+            </div>   
+        </>
     ) : "";
 }
 
