84f234687377b17bd77d98592efa9dec93afe65c
Thyago Fontenele <thyago_fonte@outlook.com>
2021-06-12 07:43:13 -0300

    organizando routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -39,115 +39,74 @@ function routes() {
     return (
         <Router>
             <Switch>
+                {/*      Acesso liberado            */}
 
-                <Route exact path="/">
-                    <Home/>
-                </Route>
+                <Route exact path="/">              <Home/>         </Route>
 
-                <Route exact path="/register">
-                    <Register/>
-                </Route>
+                <Route exact path="/register">        <Register/>   </Route>
 
-                <Route exact path="/login">
-                    <Login/>
-                </Route>
+                <Route exact path="/login">        <Login/>         </Route>
                 
-                <Route exact path="/login/forgot-password">
-                    <Forgot/>
-                </Route>
+                <Route exact path="/login/forgot-password"><Forgot/></Route>
+
+                {/*           ACESSO USUÁRIO               */}
+
+                <Route exact path="/user-page"><UserLogged/>        </Route>
+
+                <Route exact path="/user/edit"> <EditInfoUser/>     </Route>
+
+                <Route exact path="user/wallet"><WalletA/>       </Route>
+
+                {/*           ACESSO ENTREGADOR          */}
+
+                <Route exact path="/delivery-add-cnh"> <DeliveryAddVeiculo/></Route>
+                    
+                <Route exact path="/deliveryman-page"> <Deliveryman/>    </Route>
+                    
+                <Route exact path="/deriveryman/wallet">  <WalletB/>           </Route>
+
+                <Route exact path="/deliveryman/edit"> <EditInfoDeliveryman/></Route>
+
+                <Route exact path="/deliveryman/not-validated"> <NotValidatedDeliveryman/> </Route>
+
+                <Route exact path="/deliveryman-page/delivery-clicked"><DeliverymanPageDeliveryClicked/> </Route>
+
+                {/*       ACESSO DONO DE RESTAURANTE          */}
+
+                <Route exact path="/restaurant">  <Restaurant/>
+
+                <Route exact path="/restaurant/manager">     <RestaurantManager/>   </Route>
                 
-                <Route exact path="/user-page">
-                    <UserLogged/>
-                </Route>
-            
-                <Route exact path="/delivery-add-cnh">
-                    <DeliveryAddVeiculo/>
-                </Route>
+                <Route exact path="/restaurant/manager/management"><MenuManagement/></Route>
 
-                <Route exact path="/user-page/deliveryman">
-                    <Deliveryman/>
-                </Route>
+                <Route exact path="/restaurant/edit">   <EditInfoRestaurant/>       </Route>
 
-                <Route exact path="/user/edit">
-                    <EditInfoUser/>
-                </Route>
+                <Route exact path="/restaurant/create-new-plate-diolog"> <CreateNewPlateDiolog/> </Route>
 
-                <Route exact path="/restaurant/edit">
-                    <EditInfoRestaurant/>
-                </Route>
+                <Route exact path="/restaurant/results">   <Results/>   </Route>
 
-                <Route exact path="/admin/edit">
-                    <EditInfoAdmin/>
-                </Route>
+                <Route exact path="/restaurant/orders">  <Orders/>  </Route>
 
-                <Route exact path="/deliveryman/edit">
-                    <EditInfoDeliveryman/>
-                </Route>
+                <Route exact path="/restaurant/stock-management"><StockManagement/></Route>
+                
 
-                <Route exact path="/wallet-compra">
-                    <WalletA/>
-                </Route>
+                {/*         ACESSO  ADMIN                          */}
 
-                <Route exact path="/wallet-entrega">
-                    <WalletB/>
-                </Route>
+                <Route exact path="/admin/edit">  <EditInfoAdmin/>    </Route>
+               
+                <Route exact path="/admin/users"> <AdminUsers/>       </Route>
 
-                <Route exact path="/admin-users">
-                    <AdminUsers/>
-                </Route>
+                <Route exact path="/admin/deliveryman"><AdminDeliveryman/> </Route>
 
-                <Route exact path="/admin-deliveryman">
-                    <AdminDeliveryman/>
-                </Route>
+                <Route exact path="/admin/restaurant/register"> <AdminRestaurantRegistration/> </Route>
 
-                <Route exact path="/restaurant">
-                    <Restaurant/>
-                </Route>
+                {/*          ERROS       */}
 
-                <Route exact path="/admin/restaurant/register">
-                    <AdminRestaurantRegistration/>
-                </Route>
+                <Route exact path="/404">  <NotFound/>  </Route>
+                
+                <Route> <Redirect to="/404"/> </Route>
 
-                <Route exact path="/restaurant/manager">
-                    <RestaurantManager/>
                 </Route>
-
-                <Route exact path="/restaurant/manager/management">
-                    <MenuManagement/>
-                </Route>
-
-                <Route exact path="/not-validated-deliveryman">
-                    <NotValidatedDeliveryman/>
-                </Route>
-
-                <Route exact path="/deliveryman-page-delivery-clicked">
-                    <DeliverymanPageDeliveryClicked/>
-                </Route>
-
-                <Route exact path="/create-new-plate-diolog">
-                    <CreateNewPlateDiolog/>
-                </Route>
-
-                <Route exact path="/stock-management">
-                    <StockManagement/>
-                </Route>
-
-                <Route exact path="/results">
-                    <Results/>
-                </Route>
-
-                <Route exact path="/orders">
-                    <Orders/>
-                </Route>
-
-                <Route exact path="/404">
-                    <NotFound/>
-                </Route>
-
-                <Route>
-                    <Redirect to="/404"/>
-                </Route>
-
             </Switch>
         </Router>
     )
