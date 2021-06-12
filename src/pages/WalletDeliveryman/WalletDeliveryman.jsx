d5866d6db7ad0c92ec3dfe33806fd16a12bfda3b
Raphael Araújo <raphaelwilliam.araujo@gmail.com>
2021-06-12 16:06:43 -0300

    pagina wallet deliveryman

--- src/pages/WalletDeliveryman/WalletDeliveryman.jsx
+++ src/pages/WalletDeliveryman/WalletDeliveryman.jsx
@@ -1,20 +1,32 @@
 import React from 'react'
 import Header from '../../components/Header/HeaderUserLogged/HeaderUserLogged'
 import Footer from '../../components/Footer/Footer'
+import Voltar from '../../assets/images/before-point.svg'
+import './WalletDeliveryman.css'
 
 function WalletDeliveryman() {
     return (
         <div>
             <Header/>
 
-            <main>
+            <main className="wallet-deliveryman">
                 <div>
+                    <img src={Voltar} alt="" />
                     <h1 className="user-h1">Saldo disponível:</h1>
                     <p>R$ 240,00</p>
                 </div>
 
                 <div>
-                    
+                    <h3>Histórico de entregas</h3>
+                    <div>
+                        {/* 
+                        - card
+                        - card
+                        - card
+                        - card
+                        - card
+                        */}
+                    </div>
                 </div>
             </main>
 
