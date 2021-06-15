b51acf477390577d7a137f1c00ac9ffe193c8d15
Raphael Araújo <raphaelwilliam.araujo@gmail.com>
2021-06-15 10:40:36 -0300

    Cards WalletDeliveryman

--- src/pages/DeliverymanPages/WalletDeliveryman/WalletDeliveryman.jsx
+++ src/pages/DeliverymanPages/WalletDeliveryman/WalletDeliveryman.jsx
@@ -3,6 +3,7 @@ import Header from '../../../components/Header/HeaderUserLogged/HeaderUserLogged
 import Footer from '../../../components/Footer/Footer'
 import Voltar from '../../../assets/images/before-point.svg'
 import './WalletDeliveryman.css'
+import CardFood from '../../../components/Cards/CardFood/CardFood'
 
 function WalletDeliveryman() {
     return (
@@ -10,22 +11,18 @@ function WalletDeliveryman() {
             <Header/>
 
             <main className="wallet-deliveryman">
-                <div>
+                <div className="wallet-deliveryman-header">
                     <img src={Voltar} alt="" />
                     <h1 className="user-h1">Saldo disponível:</h1>
-                    <p>R$ 240,00</p>
+                    <p className="wallet-deliveryman-wallet-p">R$ 240,00</p>
                 </div>
 
                 <div>
-                    <h3>Histórico de entregas</h3>
-                    <div>
-                        {/* 
-                        - card
-                        - card
-                        - card
-                        - card
-                        - card
-                        */}
+                    <h3 className="deliveries-history-h3">Histórico de entregas</h3>
+                    <div className="wallet-deliveryman-cardfood-group">
+                        <CardFood/>
+                        <CardFood/>
+                        <CardFood/>
                     </div>
                 </div>
             </main>
